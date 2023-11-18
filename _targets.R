9484923d48af300a7f97c989e484c9ca0406a429
Corrado Caudek <corrado.caudek@unifi.it>
2023-11-18 15:11:16 +0100

    updated reliabilities with _targets.R

---
+++ _targets.R
@@ -0,0 +1,183 @@
+# _targets.R #
+
+# Created by use_targets()
+# Follow the comments below to fill in this target script.
+# Then follow the manual to check and run the pipeline:
+#   <https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline> # nolint
+
+# Loading package namespaces:
+suppressPackageStartupMessages({
+  library("here")
+  library("targets")
+  library("tarchetypes")
+  library("tidyverse")
+  library("tidyr")
+  library("purrr")
+  library("cmdstanr")
+  library("brms")
+  library("posterior")
+  library("loo")
+  library("lme4")
+  library("mice")
+  library("parallel")
+  library("emmeans")
+  library("quarto")
+  library("bayesplot") 
+  library("gridExtra")
+  library("ggdist")
+  library("viridis")
+  library("psych")
+  library("sjPlot")
+  library("rio")
+  library("lubridate")
+  library("readxl")
+  library("janitor")
+  library("semTools")
+})
+
+options(pillar.neg=FALSE)
+theme_set(bayesplot::theme_default(base_family = "sans"))
+
+# Set random seed for reproducibility:
+SEED <- 48927 
+
+# Set target options:
+tar_option_set(
+  # packages that your targets need to run
+  packages = c(
+    "here", "targets", "tarchetypes", "tidyverse", "tidyr", "purrr",
+    "cmdstanr", "brms", "posterior", "loo", "mice", "parallel", "emmeans",
+    "quarto", "bayesplot", "gridExtra", "ggdist", "effectsize", "rio",
+    "sjstats", "sjPlot", "sjmisc", "viridis", "lubridate", "readxl",
+    "janitor", "semTools"
+  ),
+  format = "rds", # default storage format
+  seed = SEED
+  # Set other options as needed.
+)
+
+# tar_make_clustermq() configuration (okay to leave alone):
+options(clustermq.scheduler = "multicore")
+
+# tar_make_future() configuration (okay to leave alone):
+# Install packages {{future}}, {{future.callr}}, and {{future.batchtools}} to allow use_targets() to configure tar_make_future() options.
+
+# Sourcing helper files:
+list.files(
+  here::here("workflows", "scripts", "ema", "functions"),
+  pattern = "\\.R$", recursive = TRUE,
+  full.names = TRUE
+) %>%
+  walk(source)
+
+
+# Replace the target list below with your own:
+# Targets:
+list(
+  
+  # Import EMA data
+  tar_target(
+    mpath_ema_data_raw,
+    import_ema_data(
+      input_folder = "m_path_data_2023", 
+      output_rds_path = here("data", "prep", "ema", "ema_data_1bis.RDS")
+    ),
+    format = "file"
+  ),
+  
+  # Data wrangling 
+  tar_target(
+    clean_mpath_ema_data,
+    process_ema_data(
+      input_rds_path = here("data", "prep", "ema", "ema_data_1bis.RDS"),
+      output_rds_path = here("data", "prep", "ema", "ema_data_2bis.RDS")
+    ),
+    format = "file"
+  ),
+  
+  # Get State Self Compassion data for both piel and mpath samples
+  tar_target(
+    state_self_comp_piel_mpath_data,
+    get_state_self_comp_piel_mpath()
+  ),
+  
+  # Compute multilevel reliabilities
+  tar_target(
+    reliabilities_sem,
+    calculate_ssc_reliabilities(state_self_comp_piel_mpath_data)
+  ),
+  
+  # Create report.
+  tar_quarto(
+    name = report,
+    path = here::here("doc", "ema_ssc_report.qmd")
+  )
+
+  # close list  
+)
+
+
+
+# 
+#   # Clean mpath EMA data. raw_df is the input to get_data() and prl_df is the output.
+#   # prl_df has 63930 rows.
+#   tar_target(prl_df, get_data(raw_df)),
+# 
+#   # Estimate the parameters of the momentary happiness model.
+#   tar_target(
+#     params_happiness_df,
+#     get_params_happiness_model(prl_df, unique(prl_df$user_id))
+#   ),
+# 
+#   # Clean parameters of the momentary happiness model.
+#   tar_target(
+#     params_happiness_clean_df,
+#     clean_params_happiness_model(params_happiness_df)
+#   )
+
+  # brms model for alpha and volatility
+  # the distribution of alpha is bimodal. No congergence!
+  # tar_target(
+  #   brms_fitted_mod_alpha,
+  #   brms_mod_alpha(params_happiness_clean_df)
+  # )
+
+  # brms model for mood_dif
+  # tar_target(
+  #   brms_fitted_mod_mood_1,
+  #   brms_mod_mood_1(params_happiness_clean_df)
+  # ),
+  
+  # brms model for mood_dif with also happiness parameters
+  # tar_target(
+  #   brms_fitted_mod_mood_2, 
+  #   brms_mod_mood_2(params_happiness_clean_df)
+  # ),
+  
+  # Fit the brms model with mood_dif as a function of time x environment.
+  # mood_pre is used as a covariate, and time is coded in terms of the linear, 
+  # quadratic, and cubic components of ema_number.
+  # tar_target(
+  #   brms_fitted_mod_mood_3,
+  #   brms_mod_mood_3(params_happiness_clean_df)
+  # ),
+  
+  # Compute predicted values of model brms_fitted_mod_mood_3 and 
+  # generate figure.
+  # tar_target(
+  #   plot_mood_dif_ema_number,
+  #   get_plot_mood_dif_ema_number(
+  #     params_happiness_clean_df, brms_fitted_mod_mood_3
+  #   )
+  # ),
+  # 
+  # Create report.
+  # tar_quarto(
+  #   name = report,
+  #   path = here::here("doc", "groundhog_day_report.qmd")
+  # )
+  
+
+
+
+
--- workflows/scripts/ema/functions/funs_ema_mpath.R
+++ workflows/scripts/ema/functions/funs_ema_mpath.R
@@ -1,3 +1,5 @@
+#' delete_first_row_xlsx() -----------------------------------------------------
+#' 
 delete_first_row_xlsx <- function(FOLDER_NAME) {
   library(readxl)
   library(writexl)
@@ -14,12 +16,6 @@ delete_first_row_xlsx <- function(FOLDER_NAME) {
     # Read the Excel file
     data <- read_excel(file, skip = 1)
 
-    # Get the original column names
-    # original_column_names <- readxl::col_names(readxl::read_excel(file, n_max = 1))
-
-    # Set the original column names to the modified data
-    # colnames(data) <- original_column_names
-
     # Write the modified data back to the Excel file
     write_xlsx(data, path = file)
 
@@ -29,51 +25,305 @@ delete_first_row_xlsx <- function(FOLDER_NAME) {
 }
 
 
-# Change the columns' names of the file with the raw data.
-change_cols_names <- function(d) {
-  # col_names <- c(
-  #   "date_time", "context",
-  #   "scs_pos_1", "happy",
-  #   "dec_2", "dec_4",
-  #   "scs_neg_5", "scs_pos_3",
-  #   "satisfied", "scs_neg_8",
-  #   "dec_3", "scs_neg_4",
-  #   "dec_1", "scs_neg_2",
-  #   "nervous", "scs_pos_6",
-  #   "upset", "scs_pos_7",
-  #   "present_emotion", "exam",
-  #   "after_exam_emotion", "exam_preparation",
-  #   "grade_exp", "before_exam_emotion",
-  #   "before_exam_emotion_intensity", "real_grade",
-  #   "after_exam_emotion_intensity", "user_id"
-  # )
-
-  col_names <- c(
-    "context", "scs_pos_1",
-    "happy", "dec_2",
-    "dec_4", "scs_neg_5",
-    "scs_pos_3", "satisfied",
-    "scs_neg_8", "dec_3",
-    "scs_neg_4", "dec_1",
-    "scs_neg_2", "nervous",
-    "scs_pos_6", "upset",
-    "scs_pos_7", "emotion_now",
-    "exam", "emotion_after_exam",
-    "exam_preparation", "grade_exp",
-    "emotion_before_exam", "emotion_intensity_before_exam",
-    "real_grade", "emotion_intensity_after_exam",
-    "user_id", "day",
-    "hour", "minute",
-    "time_window", "calendar_day",
-    "bysubj_day"
+#' import_ema_data() -----------------------------------------------------------
+#' 
+import_ema_data <- function(input_folder, output_rds_path) {
+  
+  suppressPackageStartupMessages({
+    library(psych)
+    library(ggplot2)
+    library(sjPlot)
+    library(dplyr)
+    library(rio)
+    library(here)
+    library(purrr)
+    library(lubridate)
+    library(readxl)
+    library(janitor)
+  })
+  
+  source(here::here("workflows", "scripts", "ema", "functions", "funs_ema_mpath.R"))
+  
+  complete_column_names <- c(
+    "Date and time",
+    "question_context (multipleChoice)",
+    "question8scs (multipleChoice)",
+    "question_satisfied (smiley)",
+    "question_nervous (smiley)",
+    "question_dec1 (multipleChoice)",
+    "question_happiness (smiley)",
+    "question5scs (multipleChoice)",
+    "question2scs (multipleChoice)",
+    "question_dec3 (multipleChoice)",
+    "question_sad (smiley)",
+    "question7scs (multipleChoice)",
+    "question3scs (multipleChoice)",
+    "question4scs (multipleChoice)",
+    "question_dec2 (multipleChoice)",
+    "question_dec4 (multipleChoice)",
+    "question1scs (multipleChoice)",
+    "question6scs (multipleChoice)",
+    "question_emotion (multiSmiley)",
+    "yesnoexam (yesno)",
+    "momentary_emotion_yes (multiSmiley)",
+    "exam_preparation (sliderNegPos)",
+    "grade_exp (sliderNeutralPos)",
+    "momentary_emotion_no (multiSmiley)",
+    "emotion_valence_no (smiley)",
+    "real_grade (sliderNeutralPos)",
+    "emotion_valence_yes (smiley)"
   )
-
-  names(d) <- col_names
-
-  return(d)
+  
+  FOLDER_NAME <- input_folder
+  
+  if (file.exists(here::here("data", "raw", FOLDER_NAME, "boop.txt"))) {
+    delete_first_row_xlsx(FOLDER_NAME)
+    file.remove(here::here("data", "raw", FOLDER_NAME, "boop.txt"))
+  }
+  
+  dir <- here::here("data", "raw", FOLDER_NAME)
+  file_names <- list.files(path = dir, full.names = TRUE)
+  
+  d_list <- lapply(file_names, function(file_name) {
+    d <- rio::import(file_name, header = TRUE)
+    
+    missing_cols <- setdiff(complete_column_names, names(d))
+    d[missing_cols] <- NA
+    d <- d[complete_column_names]
+    
+    d$subj_code <- substr(file_name, 1, nchar(file_name) - 5)
+    d$datetime <- strptime(d$`Date and time`, "%a, %d %b %Y %H:%M:%S")
+    d$day <- ymd(substring(d$datetime, 1, 10))
+    d$hour <- hour(d$datetime)
+    d$minute <- minute(d$datetime)
+    
+    d$time_window <- case_when(
+      d$hour >= 0 & d$hour < 12 ~ 1,
+      d$hour >= 12 & d$hour < 16 ~ 2,
+      d$hour >= 16 & d$hour < 19 ~ 3,
+      d$hour >= 19 & d$hour < 21 ~ 4,
+      d$hour >= 21 & d$hour < 24 ~ 5,
+      TRUE ~ NA_integer_
+    )
+    
+    d$calendar_day <- as.numeric(factor(d$day))
+    
+    d <- d %>%
+      mutate(bysubj_day = dense_rank(calendar_day)) %>%
+      select(-`Date and time`, -datetime) %>%
+      clean_names()
+    
+    if ("momentary_emotion_no_multi_smiley" %in% names(d)) {
+      d$momentary_emotion_no_multi_smiley <- as.character(d$momentary_emotion_no_multi_smiley)
+    }
+    
+    if ("momentary_emotion_yes_multi_smiley" %in% names(d)) {
+      d$momentary_emotion_yes_multi_smiley <- as.character(d$momentary_emotion_yes_multi_smiley)
+    }
+    
+    d
+  })
+  
+  combined_df <- bind_rows(d_list)
+  
+  saveRDS(combined_df, output_rds_path)
 }
 
 
+#' process_ema_data() ----------------------------------------------------------
+#' 
+process_ema_data <- function(input_rds_path, output_rds_path) {
+  suppressPackageStartupMessages({
+    library("psych")
+    library("ggplot2")
+    library("dplyr")
+    library("rio")
+    library("here")
+    library("lubridate")
+    library("tidyr")
+  })
+  
+  d <- readRDS(input_rds_path)
+  
+  new_column_names <- c(
+    "question_context_multiple_choice" = "context",
+    "question8scs_multiple_choice" = "scs_neg_8",
+    "question_satisfied_smiley" = "satisfied",
+    "question_nervous_smiley" = "nervous",
+    "question_dec1_multiple_choice" = "dec_1",
+    "question_happiness_smiley" = "happy",
+    "question5scs_multiple_choice" = "scs_neg_5",
+    "question2scs_multiple_choice" = "scs_neg_2",
+    "question_dec3_multiple_choice" = "dec_3",
+    "question_sad_smiley" = "upset",
+    "question7scs_multiple_choice" = "scs_pos_7",
+    "question3scs_multiple_choice" = "scs_pos_3",
+    "question4scs_multiple_choice" = "scs_neg_4",
+    "question_dec2_multiple_choice" = "dec_2",
+    "question_dec4_multiple_choice" = "dec_4",
+    "question1scs_multiple_choice" = "scs_pos_1",
+    "question6scs_multiple_choice" = "scs_pos_6",
+    "question_emotion_multi_smiley" = "emotion_now",
+    "yesnoexam_yesno" = "exam",
+    "momentary_emotion_yes_multi_smiley" = "emotion_after_exam",
+    "exam_preparation_slider_neg_pos" = "exam_preparation",
+    "grade_exp_slider_neutral_pos" = "grade_exp",
+    "momentary_emotion_no_multi_smiley" = "emotion_before_exam",
+    "emotion_valence_no_smiley" = "emotion_valence_before_exam",
+    "real_grade_slider_neutral_pos" = "real_grade",
+    "emotion_valence_yes_smiley" = "emotion_valence_after_exam",
+    "subj_code" = "user_id",
+    "day" = "day",
+    "hour" = "hour",
+    "minute" = "minute",
+    "time_window" = "time_window",
+    "calendar_day" = "calendar_day",
+    "bysubj_day" = "bysubj_day"
+  )
+  
+  d <- d %>% rename_with(~new_column_names[.x], everything())
+  
+  levels_to_numeric <- c(
+    "Molto spiacevole" = -2,
+    "Spiacevole" = -1,
+    "Né spiacevole né piacevole" = 0,
+    "Piacevole" = 1,
+    "Molto piacevole" = 2
+  )
+  
+  d$context <- d$context %>% factor(levels = names(levels_to_numeric)) %>% as.numeric()
+  
+  levels2_to_numeric <- c(
+    "Totalmente falso" = -3,
+    "Moderatamente falso" = -2,
+    "Leggermente falso" = -1,
+    "Leggermente vero" = 1,
+    "Moderatamente vero" = 2,
+    "Totalmente vero" = 3
+  )
+  
+  d <- d %>% 
+    mutate(across(starts_with("scs_") | starts_with("dec_"), ~levels2_to_numeric[.x] %>% as.numeric()))
+  
+  d <- d %>% 
+    separate(emotion_now, into = c("emo_now_1", "emo_now_2", "emo_now_3"), sep = ",\\s*", fill = "right", extra = "merge") %>%
+    separate(emotion_after_exam, into = c("emo_after_exam_1", "emo_after_exam_2", "emo_after_exam_3"), sep = ",\\s*", fill = "right", extra = "merge") %>%
+    separate(emotion_before_exam, into = c("emo_before_exam_1", "emo_before_exam_2", "emo_before_exam_3"), sep = ",\\s*", fill = "right", extra = "merge") %>%
+    mutate(across(starts_with("emo_"), ~ifelse(.x == "-1", NA, .x)))
+  
+  saveRDS(d, output_rds_path)
+}
+
+# get_state_self_comp_piel_mpath() ---------------------------------------------
+
+get_state_self_comp_piel_mpath <- function() {
+  suppressPackageStartupMessages({
+    library(tidyverse)
+    library(here)
+    library(psych)
+    library(rio)
+    library(semTools)
+  })
+  
+  # Function to process each dataset
+  process_data <- function(path) {
+    readRDS(path) |>
+      dplyr::relocate(any_of(c(starts_with("scs_neg"), starts_with("scs_pos")))) |>
+      dplyr::select(user_id, starts_with("scs_")) |>
+      mutate(across(where(is.numeric), ~ . - 4))
+  }
+  
+  # Paths to data files
+  piel_path <- 
+    "~/_repositories/ema_scs_piel/data/prep/ema/ema_data_2.RDS"
+  mpath_path <- here("data", "prep", "ema", "ema_data_2bis.RDS")
+  
+  # Process data for each dataset
+  scs_items_piel <- process_data(piel_path)
+  scs_items_mpath <- process_data(mpath_path)
+  
+  # Combine both data sets
+  both_df <- bind_rows(scs_items_piel, scs_items_mpath)
+  
+  return(both_df)
+}
+
+
+# calculate_ssc_reliabilities() ------------------------------------------------
+calculate_ssc_reliabilities <- function(both_df) {
+  library(tidyverse)
+  library(lavaan)
+  library(semTools)
+
+  process_ssc_data <- function(df, mcfa_model) {
+    suppressWarnings({
+      fit <- cfa(mcfa_model, data = df, cluster = "person")
+      comp_rel <- compRelSEM(
+        fit,
+        obs.var = FALSE, config = c("f1"), shared = "f1"
+      )
+    })
+    return(comp_rel)
+  }
+
+  # Define the multilevel SEM model
+  mcfa_model <-
+    "
+    level: 1
+    f1 =~ NA * i1 + l1 * i1 + l2 * i2 + l3 * i3 + l4 * i4
+    i1 ~~ ev1w * i1
+    i2 ~~ ev2w * i2
+    i3 ~~ ev3w * i3
+    i4 ~~ ev4w * i4
+    f1 ~~ 1 * f1
+    
+    level: 2
+    f1 =~ NA * i1 + l1 * i1 + l2 * i2 + l3 * i3 + l4 * i4
+    # fixed residual variances
+    i1 ~~ ev1b * i1
+    i2 ~~    0 * i2
+    i3 ~~ ev3b * i3
+    i4 ~~ ev4b * i4
+    f1 ~~ vf1b * f1
+    
+    # tilde omega values:
+    tilomgb := (l1 + l2 + l3 + l4)^2 * vf1b /
+      ((l1 + l2 + l3 + l4)^2 * vf1b + ev1b + 0 + ev3b + ev4b)
+    tilomgw := (l1 + l2 + l3 + l4)^2 * 1 /
+      ((l1 + l2 + l3 + l4)^2 * 1 + ev1w + ev2w + ev3w + ev4w)
+    # score reliability:
+    omg2l := (l1 + l2 + l3 + l4)^2 * (1 + vf1b) /
+      ((l1 + l2 + l3 + l4)^2 * (1 + vf1b) + 
+         ev1b + 0 + ev3b + ev4b + ev1w + ev2w + ev3w + ev4w)
+    omgb := (l1 + l2 + l3 + l4)^2 * vf1b /
+      ((l1 + l2 + l3 + l4)^2 * vf1b + ev1b + 0 + ev3b + ev4b + 
+         (ev1w + ev2w + ev3w + ev4w + (l1 + l2 + l3 + l4)^2) / 25.1)
+  "
+
+  # Prepare data
+  pos_ssc_df <- both_df %>% select(user_id, starts_with("scs_pos"))
+  neg_ssc_df <- both_df %>% select(user_id, starts_with("scs_neg"))
+  colnames(pos_ssc_df) <- c("person", "i1", "i2", "i3", "i4")
+  colnames(neg_ssc_df) <- c("person", "i1", "i2", "i3", "i4")
+
+  # Calculate reliabilities
+  pos_reliabilities <- process_ssc_data(pos_ssc_df, mcfa_model)
+  neg_reliabilities <- process_ssc_data(neg_ssc_df, mcfa_model)
+
+  # Format and return results
+  results <- list(
+    positive = pos_reliabilities,
+    negative = neg_reliabilities
+  )
+
+  return(results)
+}
+
+
+
+
+#' center3L() ------------------------------------------------------------------
+#' 
 center3L <- function(dataname, varname, idname, dayname) {
   within <- dataname %>%
     group_by({{ idname }}, {{ dayname }}) %>%
@@ -117,122 +367,3 @@ center3L <- function(dataname, varname, idname, dayname) {
 
 
 
-#--------------------------------------------------------------------
-
-# https://osf.io/fdnm7/files/osfstorage
-
-reportMLM <- function(model, decimal = 3, intercept = FALSE, format = "apa", partR2 = FALSE, showTable = TRUE) {
-  # Last modified by Hause Lin 31-10-17 20:06
-  # APA format
-  # cat("Requires p-values from lmerTest if model has been fitted using lme4\nFixed effects for MLM with effect sizes r, d, and semi-partial R (only works with lme and lmer fitted models)\n")
-  # cat("r: .10 (small), .30 (medium), .50 (large) (Cohen, 1992)\n")
-  # cat("d: 0.20 (small), 0.50 (medium), .80 (large) (Cohen, 1992)\n")
-  # cat("R2: .02 (small), .13 (medium), .26 (large) (Cohen, 1992)\n")
-
-  estimates <- data.frame(coef(summary(model)))
-  effectNames <- rownames(estimates)
-  colnames(estimates) <- tolower(colnames(estimates))
-  colnames(estimates) <- c("estimate", "se", "df", "statistic", "p")
-
-  # effect sizes (Kashdan & Steger, 2006)
-  estimates$es.r <- sqrt((estimates$statistic^2 / (estimates$statistic^2 + estimates$df))) # r
-  estimates$es.d <- (2 * estimates$statistic) / sqrt(estimates$df) # d
-
-  # if partR2 == TRUE, compute partial R square (DOESN'T WORK IF MODEL HAS CATEGORICAL VARAIBLES WITH > 2 LEVELS!)
-  if (partR2) {
-    # effect size semi partial R (Edwards et al., 2008)
-    anovaModel <- anova(model)
-    Fs <- anovaModel$F # F-values for each effect (marginal = type 3 SS with Satterthwaite (requires lmerTest package))
-    numDF <- anovaModel$NumDF # numerator DFs
-    denDF <- anovaModel$DenDF # denominator DFs
-    semiPartialREffect <- (numDF / denDF * Fs) / (1 + (numDF / denDF * Fs)) # effect sizes
-    estimates$es.partR2 <- c(NA, semiPartialREffect)
-  }
-
-  # print formatted results
-  if (intercept) {
-    startingRow <- 1 # if intercept == TRUE, then report intercept too
-  } else {
-    startingRow <- 2 # if intercept == FALSE, then don't report intercept
-  }
-
-  for (i in startingRow:nrow(estimates)) { # for each fixed effect, format output accordingly (print line by line)
-
-    # i <- i
-    estimateV <- estimates[i, "estimate"]
-    estimateV <- ifelse(abs(estimateV) < 0.01, round(estimateV, 3), round(estimateV, 2))
-    estimateSign <- sign(estimateV) # sign (negative or positive)
-    estimateV <- ifelse(abs(estimateV) < 0.01,
-      sprintf("%.3f", abs(estimateV)),
-      sprintf("%.2f", abs(estimateV))
-    ) # character (absolute value)
-
-    if (.Platform$OS.type == "unix") { # if linux/mac, ensure negative sign is minus, not hyphens
-      estimateV <- ifelse(estimateSign == -1, paste0("−", estimateV), estimateV) # if negative, add minus sign to character vector
-    } else {
-      estimateV <- ifelse(estimateSign == -1, paste0("-", estimateV), estimateV) # hyphen
-    }
-
-    seV <- estimates[i, "se"]
-    seV <- ifelse(abs(seV) < 0.01, round(seV, 3), round(seV, 2))
-    seV <- ifelse(abs(seV) < 0.01,
-      sprintf("%.3f", abs(seV)),
-      sprintf("%.2f", abs(seV))
-    ) # character
-    dfV <- round(estimates[i, "df"])
-    statisticV <- estimates[i, "statistic"]
-
-    statisticSign <- sign(statisticV)
-    statisticV <- ifelse(abs(statisticV) < 0.01,
-      sprintf("%.3f", abs(statisticV)),
-      sprintf("%.2f", abs(statisticV))
-    ) # character
-
-    if (.Platform$OS.type == "unix") { # if linux/mac, ensure negative signs is minus, not hyphens
-      statisticV <- ifelse(statisticSign == -1, paste0("−", statisticV), statisticV) # minus
-    } else {
-      statisticV <- ifelse(statisticSign == -1, paste0("-", statisticV), statisticV) # hyphen
-    }
-
-    pV <- estimates[i, "p"]
-    rV <- estimates[i, "es.r"]
-
-    if (pV >= 0.001) { # if p value is >= .001, always print exact p value
-
-      if (format == "nn") { # if nature neuro format
-
-        message(sprintf(
-          "%.0f: b = %s, SE = %s, t%.0f = %s, P = %.3f, r = %.2f",
-          i, estimateV, seV, dfV, statisticV, pV, rV
-        ))
-      } else { # else always return APA format
-
-        message(sprintf(
-          "%.0f: b = %s, SE = %s, t(%.0f) = %s, p = %s, r = %s",
-          i, estimateV, seV, dfV, statisticV, substring(sprintf("%.3f", pV), 2), substring(sprintf("%.2f", rV), 2)
-        ))
-      }
-    } else if (pV < 0.001) { # if p value < .001, then always print p < .001
-
-      pV <- 0.001
-
-      if (format == "nn") {
-        message(sprintf(
-          "%.0f: b = %s, SE = %s, t%.0f = %s, P < %.3f, r = %.2f",
-          i, estimateV, seV, dfV, statisticV, pV, rV
-        ))
-      } else {
-        message(sprintf(
-          "%.0f: b = %s, SE = %s, t(%.0f) = %s, p < %s, r = %s",
-          i, estimateV, seV, dfV, statisticV, substring(sprintf("%.3f", pV), 2), substring(sprintf("%.2f", rV), 2)
-        ))
-      }
-    }
-  }
-
-  rownames(estimates) <- paste(1:nrow(estimates), rownames(estimates)) # add row number to summary table
-
-  if (showTable) {
-    return(round(estimates, decimal))
-  }
-}
